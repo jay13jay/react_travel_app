7dbad050fc39ecef2bf711a01162b61f7e9159bb
Josh Hackney <jhaxllc@gmail.com>
2024-04-30 12:06:56 -0400

    added percent of items packed to stats page, and the code to determine the percent

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -1,8 +1,11 @@
 
 function Stats({ items }) {
+    const packed = items.filter(item => item.packed === true);
+    console.log(packed);
+    const percentPacked = (packed.length / items.length) * 100;
     return (
         <footer className="stats">
-            <em>you have { items.length } items on your list</em>
+            <em>you have { items.length } items on your list, with { percentPacked ? percentPacked.toFixed(0) : 0 }% packed</em>
         </footer>
     )
 }
